62b426f73a2c9865d35e167de831fd6f654d0094
Valentijn Hol <git@hirtol.top>
2023-09-29 15:45:11 +0200

    Make the unstable option `[TRACKED]`

--- compiler/rustc_session/src/options.rs
+++ compiler/rustc_session/src/options.rs
@@ -1393,7 +1393,7 @@ options! {
     // - src/doc/unstable-book/src/compiler-flags
 
     // tidy-alphabetical-start
-    incremental_macro_expansion: bool = (false, parse_bool, [UNTRACKED],
+    incremental_macro_expansion: bool = (false, parse_bool, [TRACKED],
         "use incremental macro expansion infrastructure (default: false)"),
     allow_features: Option<Vec<String>> = (None, parse_opt_comma_list, [TRACKED],
         "only allow the listed language features to be enabled in code (comma separated)"),
